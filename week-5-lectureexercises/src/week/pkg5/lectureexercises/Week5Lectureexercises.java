ce7fa04431c135cb29e41238d2798befc23dd6fa
haziqhalim <haziqhalim@10.164.36.207>
2023-11-06 08:53:20 +0800

    First commit

---
+++ week-5-lectureexercises/src/week/pkg5/lectureexercises/Week5Lectureexercises.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week.pkg5.lectureexercises;
+
+/**
+ *
+ * @author haziqhalim
+ */
+public class Week5Lectureexercises {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
