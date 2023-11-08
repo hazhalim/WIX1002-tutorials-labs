7449bc23cf976c115d56b604edcc00c160b016b7
haziqhalim <haziqhalim@10.164.38.222>
2023-11-08 12:14:58 +0800

    Finished Q1a)

--- Tutorial4/src/tutorial4/Tutorial4.java
+++ Tutorial4/src/tutorial4/Tutorial4.java
@@ -1,32 +1,23 @@
-package tutorial4;
-
 public class Tutorial4
 {
-
     public static void main(String[] args)
     {
-        
         /** Question 1 */
         
-        int n = 1;
+        // a)
         
-        while (true)
-        {
-            
-            if (Math.pow(n, 3) < 2000)
-            {
-                
-                n++;
-                
-            } else {
-                
-                break;
-                
-            }
-            
-        }
+//        int n = 0;
+//
+//        while ((int) Math.pow(n + 1, 3) < 2000)
+//        {
+//            
+//            n++;
+//            
+//        }
+//
+//        System.out.println("The largest integer, n, where n^3 is less than 2000 is " + n + ". The value of " + n + "^3 is: " + (int) Math.pow(n, 3));
         
-        System.out.println("The largest integer, n, where n^3 is less than 2000 is " + n + ". The value of " + n + "^3 is: " + (int) Math.pow(n, 3));
+        // b)
         
     }
     
