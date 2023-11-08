53fb0bc4706025a102b98da785f838e265779d66
haziqhalim <haziqhalim@10.164.38.222>
2023-11-08 13:00:46 +0800

    Finished question 2d)

--- Tutorial4/src/tutorial4/Tutorial4.java
+++ Tutorial4/src/tutorial4/Tutorial4.java
@@ -128,6 +128,25 @@ public class Tutorial4
         
         // d)
         
+        
+//        int i = 1;
+//        
+//        while (i <= 10)
+//        {
+//            
+//            if (i == 10)
+//            {
+//                
+//                System.out.println("Program End");
+//                
+//            }
+//            
+//            i++;
+//            
+//        }
+        
+       /** Question 3 */ 
+        
     }
     
 }
